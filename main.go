a5f7cb6cc3ae62cbf7e4faf7b48f8a8a01720b8a
msalbrain <salmanmusa2020@gmail.com>
2022-12-30 23:27:14 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,21 @@
+
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/m1guelpf/chatgpt-telegram/src/chatgpt"
+	"github.com/m1guelpf/chatgpt-telegram/src/config"
+	
+)
+
+const Sesh = `` 
+
+
+
+func main() {
+	
+	con := *config.Config{OpenAISession: ""}
+	fmt.Println(con)
+}
